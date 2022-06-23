d62d39a925a48795f5da24061e9fdad0610eb8ce
arifmp40 <86006765+arifmp40@users.noreply.github.com>
2022-06-23 13:45:13 +0600

    first commit

---
+++ src/pages/Settings/Settings.jsx
@@ -0,0 +1,37 @@
+import { Icon } from "@iconify/react";
+import React from "react";
+import Dropdown from "../../components/Dropdown/Dropdown";
+import "./Settings.scss";
+
+export default function Settings() {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return (
+    <div className="setting">
+      <Dropdown
+        tittle={"Start From"}
+        companyData={months}
+        getIndex={(e) => {
+          // setMonthNo();
+          let arr = monthArr;
+          const x = arr.slice(e);
+          setMonthNo(x.concat(arr.slice(0, e)));
+        }}
+        defaultValue={"Starts from"}
+      />
+      <Icon icon="ant-design:setting-filled" />
+    </div>
+  );
+}
